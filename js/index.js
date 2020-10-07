71ad2ae63a0bfa3d05ee9fdee74dda9eef4541f8
Josh-Burgess <joshua.k.burgess@gmail.com>
2020-10-07 14:13:13 -0400

    updates

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
 	const footer = document.querySelector('body footer');
 	const mainLanding = document.querySelector('body div.main-landing');
 	const yearSpan = document.querySelector('.year');
+	const hasDropdowns = document.querySelectorAll('.has-dropdown');
+
+	// One opened dropdown timer
+	let openDD;
 
 	yearSpan.innerHTML = new Date().getFullYear().toString();
 
@@ -16,4 +20,43 @@ document.addEventListener('DOMContentLoaded', () => {
 			footer.classList.remove('viewable');
 		}
 	});
+
+	hasDropdowns.forEach((dropdown) => {
+		dropdown.addEventListener('mouseover', () => {
+			clearTimeout(openDD);
+			dropdown.classList.add('open');
+			const dd = getDropdownMenu(dropdown);
+			const pos = dropdown.getBoundingClientRect();
+			dd.style.top = `${pos.bottom}px`;
+			dd.style.left = `${pos.left}px`;
+			dd.classList.add('open');
+
+			dd.addEventListener('mouseover', (e) => {
+				e.stopPropagation();
+				clearTimeout(openDD);
+			});
+
+			dd.addEventListener('mouseout', (e) => {
+				e.stopPropagation();
+				openDD = setTimeout(() => {
+					dropdown.classList.remove('open');
+					const dd = getDropdownMenu(dropdown);
+					dd.classList.remove('open');
+				}, 200);
+			});
+		});
+
+		dropdown.addEventListener('mouseout', () => {
+			openDD = setTimeout(() => {
+				dropdown.classList.remove('open');
+				const dd = getDropdownMenu(dropdown);
+				dd.classList.remove('open');
+			}, 200);
+		});
+	});
 });
+
+function getDropdownMenu(dropdown) {
+	const nm = Array.from(dropdown.classList).find(x => x.startsWith('dd-')).substr(3);
+	return document.querySelector(`.${nm}-dropdown`);
+}
