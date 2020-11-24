01b2544e8cf47631e6765b71089b782b5ab1e508
Josh Burgess <joshua.k.burgess@gmail.com>
2020-11-24 18:33:44 -0500

    change root element for io

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Interaction Observer Stuff
     let ioOptions = {
-        root: null,
+        root: document.documentElement,
         rootMargin: isMobile ? '2rem' : '0px',
         threshold: isMobile ? [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] : [0.0, 0.1, 0.5, 1.0]
     };
