454134499599968ebc1a2fa48aadbcb94d04c3f4
Josh Burgess <joshua.k.burgess@gmail.com>
2020-11-24 19:23:23 -0500

    lotta mobile fixes

--- js/index.js
+++ js/index.js
@@ -27,27 +27,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Interaction Observer Stuff
     let ioOptions = {
-        root: document.documentElement,
-        rootMargin: isMobile ? '2rem' : '0px',
-        threshold: isMobile ? [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] : [0.0, 0.1, 0.5, 1.0]
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.0, 0.1, 0.5, 1.0]
     };
     const ioCallback = (entries) => {
         entries.forEach(entry => {
             const isAbove = entry.boundingClientRect.y <= entry.rootBounds.y + entry.target.offsetHeight;
             if (entry.isIntersecting) {
-                if (entry.target.classList.contains('animate-bg-on-scroll')
+                if (entry.target.classList.contains('observable-anchor')
                     && ((entry.intersectionRatio >= 0.1 && !isAbove) || (entry.intersectionRatio >= 0.5 && isAbove))) {
-                    entry.target.classList.add('is-visible');
+                    entry.target.parentElement.classList.add('is-visible');
 
                     window.history.pushState(
                         {'html': document.documentElement.innerHTML},
-                        `${entry.target.getAttribute('data-title')}`,
-                        `${window.location.href.indexOf('hstyling') > 0 ? '/hstyling' : ''}${entry.target.getAttribute('data-href')}`
+                        `${entry.target.parentElement.getAttribute('data-title')}`,
+                        `${window.location.href.indexOf('hstyling') > 0 ? '/hstyling' : ''}${entry.target.parentElement.getAttribute('data-href')}`
                     );
 
-                    if (entry.target.hasAttribute('data-cost')) {
+                    if (entry.target.parentElement.hasAttribute('data-cost')) {
                         clearTimeout(openCost);
-                        costWindow.innerText = entry.target.getAttribute('data-cost');
+                        costWindow.innerText = entry.target.parentElement.getAttribute('data-cost');
                         costWindow.classList.add('show');
                     } else {
                         costWindow.classList.remove('show');
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         }, 1000);
                     }
 
-                    scheduleBtn.innerHTML = `<i class="indicator"></i> Schedule ${entry.target.getAttribute('data-button-name')}`;
+                    scheduleBtn.innerHTML = `<i class="indicator"></i> Schedule ${entry.target.parentElement.getAttribute('data-button-name')}`;
                     entry.target.classList.remove('is-above');
                 } else if (entry.target.classList.contains('show-on-scroll')) {
                     entry.target.classList.add('is-visible');
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
     const observer = new IntersectionObserver(ioCallback, ioOptions);
-    const targets = document.querySelectorAll('.show-on-scroll, .animate-bg-on-scroll');
+    const targets = document.querySelectorAll('.show-on-scroll, .observable-anchor');
 
     targets.forEach(target => {
         observer.observe(target);
